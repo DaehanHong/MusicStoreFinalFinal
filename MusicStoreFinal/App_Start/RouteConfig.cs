0f196424450cb43fff11df6da045ff7885efd544
sukhdeep <200395841@student.georgianc.on.ca>
2019-08-11 15:02:53 -0400

    AJAX CHANGES

--- MusicStoreFinal/App_Start/RouteConfig.cs
+++ MusicStoreFinal/App_Start/RouteConfig.cs
@@ -13,11 +13,15 @@ namespace MusicStore
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+   
+
+
             routes.MapRoute(
-                name: "Default",
+               name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
+
     }
 }
