7d78e1dd92a140d0ccd678c40df2189de8c8c35c
Thales Barros Fajardo Valente <thalesthalao@hotmail.com>
2019-08-11 12:54:24 -0400

    Adding comments

--- MusicStoreFinal/Scripts/app/MusicStore.js
+++ MusicStoreFinal/Scripts/app/MusicStore.js
@@ -1,3 +1,4 @@
-﻿function searchFailed() {
+﻿//personal script for the MusicSearch bar
+function searchFailed() {
     $('#searchresults').html("Sorry, there was a problem completing your search");
 }
